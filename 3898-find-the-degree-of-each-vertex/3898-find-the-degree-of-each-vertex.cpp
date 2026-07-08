ad573d86c06313dc09088b256896e18b92350d67
Shashank17singh <152907288+Shashank17singh@users.noreply.github.com>
2026-07-08 23:33:22 +0530

    Time: 3 ms (52.16%), Space: 54.4 MB (44.46%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.cpp
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.cpp
@@ -4,11 +4,7 @@ public:
         int n=matrix.size();
         vector<int>ans(n,0);
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==1){
-                    ans[i]++;
-                }
-            }
+            ans[i]=count(matrix[i].begin(),matrix[i].end(),1);
         }
         return ans;
     }
