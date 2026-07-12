62a1035194523ab40c97bc6fe9cdc95de12346c1
Shashank17singh <152907288+Shashank17singh@users.noreply.github.com>
2026-07-13 01:47:51 +0530

    Time: 0 ms (100%), Space: 13.9 MB (84.68%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     void fun(vector<int>&arr,int n,int idx,vector<int>&diary,int sum,vector<vector<int>>&res,int target){
-        if(idx==n){
-            if(sum==target) res.push_back(diary);
-            return;
+        if(sum==target) res.push_back(diary);
+        if(sum>target) return;
+        for(int i=idx;i<n;i++){
+            if(i>idx && arr[i-1]==arr[i]) continue;
+            if(arr[i]+sum<=target){
+                diary.push_back(arr[i]);
+                sum+=arr[i];
+                fun(arr,n,i+1,diary,sum,res,target);
+                diary.pop_back();
+                sum-=arr[i];
         }
-        int nxt_idx=idx+1;
-        while(nxt_idx<n && arr[nxt_idx]==arr[idx]) nxt_idx++;
-        fun(arr,n,nxt_idx,diary,sum,res,target);
-        if(arr[idx]+sum<=target){
-            diary.push_back(arr[idx]);
-            sum+=arr[idx];
-            fun(arr,n,idx+1,diary,sum,res,target);
-            diary.pop_back();
-            sum-=arr[idx];
         }
         return;
     }
