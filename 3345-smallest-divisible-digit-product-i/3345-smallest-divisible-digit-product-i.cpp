f2a962443f923348fb3efabdfb5999e1774f504a
Shashank17singh <152907288+Shashank17singh@users.noreply.github.com>
2026-08-11 03:16:31 +0530

    Time: 0 ms (100%), Space: 8.5 MB (74.47%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int smallestNumber(int n, int t) {
         while(true){
-        int num=n;
-        int prod=1;
-        while(num>0){
-            if(num%10==0) continue;
-            prod=(num%10)*prod;
-            num=num/10;
-        }
-        if(prod%t==0) return n;
-        n++;
+            int num=n;
+            int prod=1;
+            while(num>0){
+                prod=(num%10)*prod;
+                num=num/10;
+            }
+            if(prod%t==0) return n;
+            n++;
         }
     }
 };
