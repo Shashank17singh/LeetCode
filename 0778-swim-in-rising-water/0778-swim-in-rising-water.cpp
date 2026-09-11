3476d215106c84ef0454f134e3256565aee0cc63
Shashank17singh <152907288+Shashank17singh@users.noreply.github.com>
2026-09-11 16:40:14 +0530

    Time: 3 ms (87.71%), Space: 12.8 MB (74.56%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -4,51 +4,38 @@ public:
         if(i<0 || i>=n || j<0 || j>=m) return false;
         return true;
     }
-    bool bfs(vector<vector<int>>&a,int n,int m,int level){
-        int i,j;
-        int x[4]={-1,1,0,0};
-        int y[4]={0,0,-1,1};
-        queue<pair<int,int>>q;
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        q.push({0,0});
-        vis[0][0]=1;
-        while(!q.empty()){
-            pair<int,int>p=q.front();
-            q.pop();
-            int row=p.first;
-            int col=p.second;
-            if(row==n-1 && col==m-1) return true;
-            for(int k=0;k<4;k++){
-                int r=row+x[k];
-                int c=col+y[k];
-                if(valid(r,c,n,m) && vis[r][c]==0 && level>=a[r][c]){
-                    q.push({r,c});
-                    vis[r][c]=1;
-                }
-            }
-        }
-        return false;
-    }
     int swimInWater(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int low=grid[0][0];
-        int high=grid[0][0];
-        int i,j;
-        for(i=0;i<n;i++){
-            for(j=0;j<m;j++){
-                high=max(high,grid[i][j]);
+        int x[4]={-1,1,0,0};
+        int y[4]={0,0,-1,1};
+       priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+       vector<vector<int>>res(n);
+       int i,j;
+       for(i=0;i<n;i++){
+        vector<int>t(m,1e8);
+        res[i]=t;
+       }
+       pq.push({grid[0][0],{0,0}});
+       res[0][0]=grid[0][0];
+       while(!pq.empty()){
+        pair<int,pair<int,int>>p=pq.top();
+        pq.pop();
+        int level=p.first;
+        int row=p.second.first;
+        int col=p.second.second;
+        if(level>res[row][col]) continue;
+        for(int k=0;k<4;k++){
+            int r=row+x[k];
+            int c=col+y[k];
+            if(!valid(r,c,n,m)) continue;
+            int newlevel=max(level,grid[r][c]);
+            if(newlevel<res[r][c]){
+                res[r][c]=newlevel;
+                pq.push({newlevel,{r,c}});
             }
         }
-        int res=0;
-        while(low<=high){
-            int guess=(low+high)/2;
-            if(bfs(grid,n,m,guess)){
-                res=guess;
-                high=guess-1;
-            }
-            else low=guess+1;
-        }
-        return res;
+       }
+       return res[n-1][m-1];
     }
 };
