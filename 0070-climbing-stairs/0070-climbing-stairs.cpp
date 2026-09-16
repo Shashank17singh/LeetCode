bb9750f9cf62ce0c3530055c8be6401b83fc7c29
Shashank17singh <152907288+Shashank17singh@users.noreply.github.com>
2026-09-16 23:51:01 +0530

    Time: 0 ms (100%), Space: 8.3 MB (56.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,11 +5,8 @@ public:
         if(i==n) return 1;
         if(i>n) return 0;
         if(dp.find(i)!=dp.end()) return dp[i];
-        int a1=fun(i+1,n);
-        int a2=fun(i+2,n);
-        int ans=a1+a2;
-        dp[i]=ans;
-        return ans;
+        dp[i]=fun(i+1,n)+fun(i+2,n);
+        return fun(i+1,n)+fun(i+2,n);
     }
     int climbStairs(int n) {
         return fun(0,n);
