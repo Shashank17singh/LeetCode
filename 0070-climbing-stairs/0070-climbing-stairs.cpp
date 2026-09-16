2a3117049b655f03ee10b7f14e4037679339d22f
Shashank17singh <152907288+Shashank17singh@users.noreply.github.com>
2026-09-16 23:29:32 +0530

    Time: 2 ms (5.04%), Space: 8.4 MB (56.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     unordered_map<int,int>dp;
+    int fun(int i,int n){
+        if(i==n) return 1;
+        if(i>n) return 0;
+        if(dp.find(i)!=dp.end()) return dp[i];
+        int a1=fun(i+1,n);
+        int a2=fun(i+2,n);
+        int ans=a1+a2;
+        dp[i]=ans;
+        return ans;
+    }
     int climbStairs(int n) {
-        if(n==0 or n==1) return 1;
-        if(dp.find(n)!=dp.end()) return dp[n];
-        dp[n]=climbStairs(n-1)+climbStairs(n-2);
-        return climbStairs(n-1)+climbStairs(n-2);
+        int i;
+        return fun(i,n);
     }
 };
