e7a7250975b9acbdc68f731e8a86c3a1b006bf53
Shashank17singh <152907288+Shashank17singh@users.noreply.github.com>
2026-09-17 01:28:13 +0530

    Time: 0 ms (100%), Space: 11.2 MB (6.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,9 +4,7 @@ public:
         if(i==n) return 0;
         if(dp[i][free]!=-1) return dp[i][free];
         if(free==0) return dp[i][free]=fun(a,n,i+1,1,dp);
-        int c1=a[i]+fun(a,n,i+1,0,dp);
-        int c2=fun(a,n,i+1,1,dp);
-        return dp[i][free]=max(c1,c2);
+        return dp[i][free]=max(a[i]+fun(a,n,i+1,0,dp),fun(a,n,i+1,1,dp));
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
