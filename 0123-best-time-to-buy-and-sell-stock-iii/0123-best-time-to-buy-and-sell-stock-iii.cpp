a1886fec982ba652a70af421ad3fb73af4da9401
Shashank17singh <152907288+Shashank17singh@users.noreply.github.com>
2026-09-20 03:08:01 +0530

    Time: 0 ms (100%), Space: 79.1 MB (99.36%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int buy1=-prices[0];
-        int sell1=0;
-        int buy2=-prices[0];
-        int sell2=0;
-        for (int i=1;i<n;i++) {
-            int price=prices[i];
-            buy1=max(buy1,-price);
-            sell1=max(sell1,buy1+price);
-            buy2=max(buy2,sell1-price);
-            sell2=max(sell2,buy2+price);
+    int fun(vector<int>&a,int n,int i,int k,vector<vector<int>>&dp){
+        if(i==n || k==0) return 0;
+        if(dp[i][k]!=-1) return dp[i][k];
+        if(k%2==0){
+            int c1=fun(a,n,i+1,k-1,dp)-a[i];
+            int c2=fun(a,n,i+1,k,dp);
+            return dp[i][k]=max(c1,c2);
         }
+        else{
+            int c1=fun(a,n,i+1,k-1,dp)+a[i];
+            int c2=fun(a,n,i+1,k,dp);
+            return dp[i][k]=max(c1,c2);
+        }
+        return 0;
+    }
+    int maxProfit(vector<int>& prices) {
+        int buy1 = -prices[0];
+        int sell1 = 0;
+        int buy2 = -prices[0];
+        int sell2 = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            int price = prices[i];
+            buy1 = max(buy1, -price);
+            sell1 = max(sell1, buy1 + price);
+            buy2 = max(buy2, sell1 - price);
+            sell2 = max(sell2, buy2 + price);
+        }
+
         return sell2;
     }
 };
