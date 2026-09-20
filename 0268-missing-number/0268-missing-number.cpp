24e3f0f784b226ee9de20f5e4689cd1f620d5eb0
Shashank17singh <152907288+Shashank17singh@users.noreply.github.com>
2026-09-21 01:13:05 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.29%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        int num;
-        unordered_map<int,int>f(n);
+        int num=0;
         int i;
-        for(i=0;i<=n;i++) f.insert({i,0});
-        for(i=0;i<n;i++) f[nums[i]]=1;
-        for(i=0;i<=n;i++){
-            if(f[i]==0) return i;
-        }
-        return 0;
+        for(i=0;i<=n;i++) num=num^i;
+        for(i=0;i<n;i++) num=num^nums[i];
+        return num;
     }
 };
